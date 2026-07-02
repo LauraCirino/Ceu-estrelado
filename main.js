734cfafc2f0a034a1402ca40b8e57ef25fad9015
Laura Cirino De Castro Guimaraes <lauracirinod@gmail.com>
2026-07-02 18:36:10 -0300

    fist commit

---
+++ main.js
@@ -0,0 +1,11 @@
+const container = document.getElementById("container");
+
+for(let i = 0; i < 1000; i++){
+    const star = document.createElement("div");
+    star.classList.add("star");
+    star.style.backgroundColor = "white";
+    star.style.position = "absolute";
+    star.style.left = `${Math.random()*window.innerWidth}px`;
+    star.style.top = `${Math.random()*window.innerHeight}px`;
+    container.appendChild(star);
+}
